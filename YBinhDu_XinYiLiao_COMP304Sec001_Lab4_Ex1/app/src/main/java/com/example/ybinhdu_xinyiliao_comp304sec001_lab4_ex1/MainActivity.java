065091d2b29a215223d05d1d3eacb77cfc77edcf
xinyiliao007 <58014609+xinyiliao007@users.noreply.github.com>
2021-07-11 02:35:46 -0400

    Login and Main activity Commit

--- YBinhDu_XinYiLiao_COMP304Sec001_Lab4_Ex1/app/src/main/java/com/example/ybinhdu_xinyiliao_comp304sec001_lab4_ex1/MainActivity.java
+++ YBinhDu_XinYiLiao_COMP304Sec001_Lab4_Ex1/app/src/main/java/com/example/ybinhdu_xinyiliao_comp304sec001_lab4_ex1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ybinhdu_xinyiliao_comp304sec001_lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void LoginButton(View v){
+        if(v.getId() == R.id._btnLogin)
+        {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
