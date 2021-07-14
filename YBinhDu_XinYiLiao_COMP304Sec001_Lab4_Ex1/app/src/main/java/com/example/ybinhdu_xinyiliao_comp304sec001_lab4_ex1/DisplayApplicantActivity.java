9db1073f29ee9605347e343e5daf653babaa0e4e
YBinhDu <ybinhdu@gmail.com>
2021-07-13 23:02:45 -0400

    update on display

--- YBinhDu_XinYiLiao_COMP304Sec001_Lab4_Ex1/app/src/main/java/com/example/ybinhdu_xinyiliao_comp304sec001_lab4_ex1/DisplayApplicantActivity.java
+++ YBinhDu_XinYiLiao_COMP304Sec001_Lab4_Ex1/app/src/main/java/com/example/ybinhdu_xinyiliao_comp304sec001_lab4_ex1/DisplayApplicantActivity.java
@@ -46,18 +46,29 @@ public class DisplayApplicantActivity extends AppCompatActivity {
         Applicant dbApplicant = applicants.get(0);
 
 //        Toast.makeText(getApplicationContext(),"Dlength " + dbApplicant.getFirstName() + dbApplicant.getLastName(), Toast.LENGTH_SHORT).show();
-//        txtApplicantId = findViewById(R.id.txtView_ApplicantId);
-//        txtApplicantId.setText(dbApplicant.getFirstName());
-//
-        ((TextView) findViewById(R.id.txtView_ApplicantId)).setText(dbApplicant.getFirstName());
-//
-//        txtApplicantId = findViewById(R.id.txtView_ApplicantId);
-//        txtExaminerId = findViewById(R.id.txtView_ExaminerId);
-//        txtFirstName = findViewById(R.id.txtView_AppFirstName);
-//        txtTestCenter = findViewById(R.id.txtView_TestCenter);
-//        txtLastName = findViewById(R.id.txtView_App_LastName);
-//
-//        txtFirstName.setText(dbApplicant.getFirstName());
+
+
+        applicantId = dbApplicant.getApplicantId();
+        ((TextView) findViewById(R.id.txtView_ApplicantId)).setText(String.valueOf(applicantId));
+
+
+        app_FirstName = dbApplicant.getFirstName();
+        txtFirstName =(TextView) findViewById(R.id.txtView_AppFirstName);
+        txtFirstName.setText(app_FirstName);
+
+        app_LastName = dbApplicant.getLastName();
+        txtLastName = (TextView) findViewById(R.id.txtView_App_LastName);
+        txtLastName.setText(app_LastName);
+
+        testCenter = dbApplicant.getTestCenter();
+        txtTestCenter = (TextView) findViewById(R.id.txtView_TestCenter);
+        txtTestCenter.setText(testCenter);
+
+        examinerId = dbApplicant.getExaminerId();
+        txtExaminerId = (TextView) findViewById(R.id.txtView_ExaminerId);
+        txtExaminerId.setText(String.valueOf(examinerId));
+
+
 
     }catch (Exception e){
         Toast.makeText(getApplicationContext(),"not good " + e.toString(), Toast.LENGTH_SHORT).show();
@@ -65,74 +76,6 @@ public class DisplayApplicantActivity extends AppCompatActivity {
     }
 
 
-        /*
-        if(NewApplicantActivity.appDB == null){
-            Toast.makeText(getApplicationContext(),"Database does not exist or some errors happens", Toast.LENGTH_SHORT).show();
-        } else {
-            //appDB = NewApplicantActivity.appDB; //Error might be here, the database is actually closed after we exist the previous page!
-            //Or maybe just that we're not reading the DB in the correct way
-
-        }
-
-        //initiating a shared preference object
-        SharedPreferences myPref = getSharedPreferences("MyShared", MODE_PRIVATE);
-
-        applicantId = myPref.getInt("ApplicantId", 0);
-
-
-        List<Applicant> applicants = appDB.appDao().getApplicant(applicantId);
-        //List<Applicant> applicants = appDB.appDao().getAllApplicant();
-        //txtApplicantId.setText(applicantId);
-        //((TextView) findViewById(R.id.txtView_ApplicantId)).setText(applicantId);
-
-
-        /*app_FirstName = myPref.getString("App_FirstName", "");
-        ((TextView) findViewById(R.id.txtView_AppFirstName)).setText(app_FirstName);
-
-        app_LastName = myPref.getString("App_LastName", "");
-        ((TextView) findViewById(R.id.txtView_App_LastName)).setText(app_LastName);
-
-        testCenter = myPref.getString("Test_Center", "");
-        ((TextView) findViewById(R.id.txtView_TestCenter)).setText(testCenter);
-
-        examinerId = myPref.getString("ExaminerId", "");
-        ((TextView) findViewById(R.id.txtView_ExaminerId)).setText(examinerId);*/
-
-        //txtApplicantId = findViewById(R.id.txtView_ApplicantId);
-        //txtExaminerId = findViewById(R.id.txtView_ExaminerId);
-        //txtFirstName = findViewById(R.id.txtView_AppFirstName);
-        //txtTestCenter = findViewById(R.id.txtView_TestCenter);
-        //txtLastName = findViewById(R.id.txtView_App_LastName);
-//        TextView txtTesting = findViewById(R.id._testingTextview);
-//
-//
-//        String app = "";
-//        if(applicants.isEmpty()){
-//            app = "List empty???";
-//        } else {
-//            app ="there are records in list";
-//        }
-
-
-        //String text = "";
-
-        //for (Applicant applicant : applicants){
-            //applicantId = applicant.getApplicantId();
-        //    app_FirstName = applicant.getFirstName();
-        //    app_LastName = applicant.getLastName();
-        //    testCenter = applicant.getTestCenter();
-        //    examinerId = applicant.getExaminerId();
-        //    //txtFirstName.setText(app_FirstName);
-        //    text = applicantId + app_FirstName + app_LastName + testCenter + examinerId;
-
-        //}
-
-        //txtTesting.setText(app);
-        //txtApplicantId.setText(applicantId);
-        //txtFirstName.setText(app_FirstName);
-        //txtLastName.setText(app_LastName);
-        //txtTestCenter.setText(testCenter);
-        //txtExaminerId.setText(examinerId);
 
     }
 
